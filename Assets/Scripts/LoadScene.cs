5de86e482416f468e102ad3579c5f6e383dc5033
Collin Eng <eng.engineereng@gmail.com>
2023-01-07 14:57:52 -0800

    add unity tool kit, add scenes for level select

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public string sceneName;
+
+    public void buttonPressed()
+    {
+        loadSceneByName(sceneName);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    private void loadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
