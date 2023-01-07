f14b67320e7284dd6d617857b54caad1a7b363cd
Collin Eng <eng.engineereng@gmail.com>
2023-01-06 23:04:51 -0800

    add player attack; player faces in 4 directions

--- Assets/PlayerMover.cs
+++ Assets/PlayerMover.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
+    public enum Facing {Right, Up, Left, Down};
+    public Facing facing; // 0 = right, 1 = up, 2 = left, 3 = down
 
     public float movementSpeed = .5f;
 
@@ -22,6 +24,32 @@ public class PlayerMover : MonoBehaviour
     {
         // Movement
         rb.MovePosition(rb.position + movement * movementSpeed * Time.deltaTime); // move rigid body to new position
+        // transform.LookAt(transform.position + rb.velocity);
+        if (movement.x > 0) {
+            facing = Facing.Right;
+            SetFacing();
+        } else if (movement.x < 0) {
+            facing = Facing.Left;
+            SetFacing();
+        } else if (movement.y > 0) {
+            facing = Facing.Up;
+            SetFacing();
+        } else if (movement.y < 0) {
+            facing = Facing.Down;
+            SetFacing();
+        }
+    }
 
+    private void SetFacing()
+    {
+        if (facing == Facing.Right) { // right
+            rb.rotation = 0;
+        } else if (facing == Facing.Up) { // up
+            rb.rotation = 90;
+        } else if (facing == Facing.Left) { // left
+            rb.rotation = 180;
+        } else if (facing == Facing.Down) { // down
+            rb.rotation = -90;
+        }
     }
 }
