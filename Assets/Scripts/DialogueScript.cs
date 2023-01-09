0391f8f8b6906b58ead751410c6f5e7ebdf5bc76
Brayden Shinkawa <braydenshinkawa@gmail.com>
2023-01-09 01:01:36 -0800

    -Added DialogueTrigger and DialogueUIBox classes -DialogueTrigger holds the dialogue information that will be sent to the DialogueUIBox when triggered -DialogueUIBox displays the NPC's name and necessary text sent by dialogueTrigger -Reaper holds the DialogueUIBox so dialogue box follows Reaper throughout the Scenes -When dialogue is open, all entities added to the dialogueTrigger will be disabled until the dialogue is closed

---
+++ Assets/Scripts/DialogueScript.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueScript : MonoBehaviour
+{
+    public GameObject messageBox;
+    public List<MonoBehaviour> entities;
+    public TextMeshProUGUI textComponent;
+    public TextMeshProUGUI nameComponent;
+
+    [SerializeField]
+    public Message[] lines;
+    public float textSpeed;
+
+    private int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO: move this to a trigger for when level starts
+        messageBox.SetActive(true);
+        textComponent.text = string.Empty;
+
+        foreach (MonoBehaviour entity in entities)
+            entity.enabled = false;
+
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index].sentence)
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index].sentence;
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+    IEnumerator TypeLine()
+    {
+        nameComponent.text = lines[index].name;
+        foreach (char c in lines[index].sentence.ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            foreach (MonoBehaviour entity in entities)
+                entity.enabled = true;
+
+            messageBox.SetActive(false);
+        }
+    }
+
+    [System.Serializable]
+    public class Message {
+        public string name;
+        public string sentence;
+    }
+}
