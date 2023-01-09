fb1fa4cb4f593a39b0e61ad8f121906d026c04be
MonsieurDuck <dunstanwang@gmail.com>
2023-01-09 02:19:26 -0800

    Revert "Merge branch 'develop' into addingSoundEffectsToScript"
    This reverts commit c4424a112432b8140e928f53a670184357b5f357, reversing
    changes made to ed1b07cde4c5a3232ebe158b4a1d86bc89f675cd.

--- Assets/Hurtable.cs
+++ Assets/Hurtable.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class Hurtable : MonoBehaviour
 {
-    public UnityEvent onDeath;
+    
     [SerializeField] public float health = 100;
     public GameObject hurtableObject;
     public SpriteRenderer sprite;
@@ -42,7 +41,6 @@ public class Hurtable : MonoBehaviour
             hurtableObject.tag = "Dead";
             hurtableObject.layer = deadLayer;
             sprite.enabled = false;
-            onDeath.Invoke();
             yield return new WaitForSeconds(6.0f);
             Die();
         }
--- Assets/SoulFire.cs
+++ Assets/SoulFire.cs
@@ -9,10 +9,8 @@ public class SoulFire : MonoBehaviour
         Debug.Log("Collision!");
         if (collision.gameObject.layer == LayerMask.NameToLayer("Souls"))
         {
-            collision.gameObject.TryGetComponent<Hurtable>(out Hurtable component);
             Debug.Log("Killed soul");
-            component.TakeDamage(10000);
-            // Destroy(collision.gameObject); // fry the soul
+            Destroy(collision.gameObject); // fry the soul
             // TODO: add animation of soul burning? Maybe dramatic explosion
         }
     }
