7370e1ac8e5270bc0d72650a35a89440dda51df1
Collin Eng <eng.engineereng@gmail.com>
2023-01-09 00:09:30 -0800

    Level blackboxing / prototypes (#12)
    * add onGoalTriggered callback to Goal
    
    * add proto level 1 without obstacle
    
    * add goal prefab, level 2
    
    * wip level 1
    
    * apply zombie changes to zombie prefab
    
    * improved geometry of level 1 and 2
    
    * improve level 1 geometry, tweak sheep obstacle detection
    
    * lvl 1: adjust goal hitbox
    
    * Goal prefabs are now triggers
    
    * add goal to level 2
    
    * level 1 finished
    
    * add hurtable to zombie prefab
    
    * wip level 3, game title screen work
    
    * add level 3 to build settings
    
    * wip levels; fix sheep prefab collision box
    
    * levels 3 and 4 done
    
    * add level 4 to build scene
    
    * undo change to skelly prefab

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Goal : MonoBehaviour
 {
+    public UnityEvent onGoalTriggered;
+
     void OnTriggerEnter2D (Collider2D other) 
     {
         if (other.TryGetComponent<HasScore>(out var otherHasScore))
         {
             Debug.Log("Scored: " + otherHasScore.score + "points!");
+            onGoalTriggered.Invoke();
             Destroy(other.gameObject); // destroy the moving sheep
             // TODO maybe spawn a grazing sheep?
         }
