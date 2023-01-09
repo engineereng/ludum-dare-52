8096197565d23c322349a891f1bf522828d79b4d
MonsieurDuck <dunstanwang@gmail.com>
2023-01-08 17:53:21 -0800

    fix attack invisible soul bug and removed scene and user settings from push
    - invisible bug fixed
    - removed extra box collider for sheep

--- Assets/AI/SoulAI.cs
+++ Assets/AI/SoulAI.cs
@@ -19,10 +19,10 @@ public class SoulAI : MonoBehaviour
 
     private void PerformDetection()
     {
-        Debug.Log("performing detection");
+        // Debug.Log("performing detection");
         foreach(Detector detector in detectors)
         {
-            Debug.Log("performing detection");
+            // Debug.Log("performing detection");
             detector.Detect(aiData);
         }
     }
